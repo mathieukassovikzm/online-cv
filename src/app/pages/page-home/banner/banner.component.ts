b28fbc82dfa548dcd1f89df4892797c2dcb048e0
Mathieu <mathieu@mac.home>
2025-09-05 09:20:34 +0200

    feat: banner avec typing effect

--- src/app/pages/page-home/banner/banner.component.ts
+++ src/app/pages/page-home/banner/banner.component.ts
@@ -3,13 +3,52 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
 })
 export class BannerComponent implements OnInit {
+  public text: string = '';
+  public phrases = ['web applications', 'backend applications'];
+  public typeSpeed: number = 100; // ms per character
+  public deleteSpeed: number = 50; // ms per character
+  public delayBetween: number = 2000; // pause before deleting
 
-  constructor() { }
+  public displayedText: string = '';
+  private phraseIndex: number = 0;
+  private charIndex: number = 0;
+  private deleting: boolean = false;
+
+  constructor() {}
 
   ngOnInit() {
+    this.typeLoop();
   }
 
+  private typeLoop(): void {
+    const currentPhrase = this.phrases[this.phraseIndex];
+
+    if (!this.deleting) {
+      // typing
+      if (this.charIndex < currentPhrase.length) {
+        this.displayedText += currentPhrase.charAt(this.charIndex);
+        this.charIndex++;
+        setTimeout(() => this.typeLoop(), this.typeSpeed);
+      } else {
+        // full word typed → wait before deleting
+        this.deleting = true;
+        setTimeout(() => this.typeLoop(), this.delayBetween);
+      }
+    } else {
+      // deleting
+      if (this.charIndex > 0) {
+        this.displayedText = currentPhrase.substring(0, this.charIndex - 1);
+        this.charIndex--;
+        setTimeout(() => this.typeLoop(), this.deleteSpeed);
+      } else {
+        // move to next phrase
+        this.deleting = false;
+        this.phraseIndex = (this.phraseIndex + 1) % this.phrases.length;
+        setTimeout(() => this.typeLoop(), this.typeSpeed);
+      }
+    }
+  }
 }
