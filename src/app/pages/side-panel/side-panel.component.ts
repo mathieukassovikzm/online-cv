0e666bbd7e2a445b7a8481fec9c64638c87ffcc3
Mathieu <mathieu@mac.home>
2025-09-06 20:26:01 +0200

    feat: button extend info phone

--- src/app/pages/side-panel/side-panel.component.ts
+++ src/app/pages/side-panel/side-panel.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-panel.component.scss'],
 })
 export class SidePanelComponent implements OnInit {
+  public isBannerInfosExtended = false;
   constructor() {}
   ngOnInit(): void {}
+
+  public extendBannerInfos(): void {
+    this.isBannerInfosExtended = !this.isBannerInfosExtended;
+  }
 }
