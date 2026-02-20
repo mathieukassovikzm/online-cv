5a0714513659202cecbc098502563fdbda25a0ea
Mathieu Le Bellec <macpromath@mac.home>
2026-02-20 15:57:25 +0100

    fix: correction style button expand side panel

--- src/app/pages/side-panel/side-panel.component.ts
+++ src/app/pages/side-panel/side-panel.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { SvgDownloadComponent } from 'src/app/shared/svgs/ui/svg-download/svg-download.component';
+import { Component, signal } from '@angular/core';
 import { InfosLanguagesComponent } from './infos-languages/infos-languages.component';
 import { InfosPersoComponent } from './infos-perso/infos-perso.component';
 import { InfosSkillsComponent } from './infos-skills/infos-skills.component';
@@ -8,20 +7,19 @@ import { TopDescComponent } from './top-desc/top-desc.component';
 
 const modules = [CommonModule];
 const components = [TopDescComponent, InfosPersoComponent, InfosLanguagesComponent, InfosSkillsComponent];
-const svgs = [SvgDownloadComponent];
 
 @Component({
   selector: 'app-side-panel',
   templateUrl: './side-panel.component.html',
   styleUrls: ['./side-panel.component.scss'],
-  imports: [...modules, ...svgs, ...components],
+  imports: [...modules, ...components],
   standalone: true
 })
 export class SidePanelComponent {
-  public isBannerInfosExtended = false;
+  public isBannerInfosExtended = signal(false);
   constructor() { }
 
   public extendBannerInfos(): void {
-    this.isBannerInfosExtended = !this.isBannerInfosExtended;
+    this.isBannerInfosExtended.set(!this.isBannerInfosExtended());
   }
 }
